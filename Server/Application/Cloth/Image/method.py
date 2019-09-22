18e0cd0e0e1bb3021485aee3774c1ddb44482e0d
parkjinhong03 <jinhong0719@naver.com>
2019-09-22 15:49:17 +0900

    이미지에 대한 url을 생성해주는 Resource 생성

--- Server/Application/Cloth/Image/method.py
+++ Server/Application/Cloth/Image/method.py
@@ -4,8 +4,7 @@ from flask import send_file
 
 
 def get(type, number):
-
-    type_list = ['Shirts', 'Pants', 'Accessory', 'Shoes', 'Else']
+    type_list = ['Shirts', 'Pants', 'Accessory', 'Shoes']
     if type not in type_list:
         return {'Message': 'type 경로를 Shirts, Pants, Accessory, Shoes, Else 중 하나로 해주세요'}
     return send_file(f'Data/Image/{type}/{number}.jpg')
--- Server/Application/Cloth/Image/views.py
+++ Server/Application/Cloth/Image/views.py
@@ -12,7 +12,7 @@ cloth_namespace = api.namespace('Cloth', description='APIs for Request clothes')
 @cloth_namespace.route('/Image/<string:type>/<int:number>')
 class Image(Resource):
     @api.doc(
-        description='HTML 코드에서 img 태그의 src 속성에 넣은 이미지 URL을 생성해주는 API (직접적으로 이 API에 접근할 필요가 없음)'
+        description='HTML 코드에서 img 태그의 src 속성에 넣은 이미지 URL을 생성해주는 API (직접적으로 이 API에 접근하면 이미지를 반환함)'
     )
     def get(self, type, number):
         return method.get(type, number)
