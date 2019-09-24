b9c29446e5f7141b83ec8e45e84e65d07874f40f
parkjinhong03 <jinhong0719@naver.com>
2019-09-24 17:29:13 +0900

    DB 모델링 완료

--- Server/Application/Cloth/Registration/method.py
+++ Server/Application/Cloth/Registration/method.py
@@ -2,8 +2,10 @@ import RequestParser
 from db_connect import db, cursor
 from flask import request
 from werkzeug import security
+from flask_jwt_extended import get_jwt_identity
 from flask_restplus import reqparse
 import werkzeug
+import os
 
 
 def post(cloth_type):
@@ -12,14 +14,18 @@ def post(cloth_type):
     :return: status code
     410 - {type} path에 Shirts, Shoes, Pants, Accessory 이외의 값을 줌
     411 - 전달한 파일의 확장자가 jpg, png 외의 파일임
+    412 - price 값에 문자열이 포함되어있음
+    413 - size 값으로 올바른 데이터을 넣지 않았음
     '''
 
     type_list = ['Shirts', 'Shoes', 'Pants', 'Accessory']
     if cloth_type not in type_list:
         return {"message": "{type} path에 Shirts, Shoes, Pants, Accessory 이외의 값을 줌", "code": 410}, 410
 
-    cloth_description, cloth_price, cloth_size = RequestParser.parser('description', 'price', 'size')
-    print(cloth_type, cloth_description, cloth_price, cloth_size)
+    user_name = get_jwt_identity()
+
+    cloth_title, cloth_description, cloth_price, cloth_size, first_date = RequestParser.parser('title', 'description', 'price', 'size', 'first_date')
+    print(user_name, cloth_title, cloth_description, cloth_price, cloth_size, first_date)
 
     parse = reqparse.RequestParser()
     parse.add_argument('file', type=werkzeug.datastructures.FileStorage, location='files')
@@ -28,15 +34,43 @@ def post(cloth_type):
 
     file_type = audioFile.filename.split('.')[1]
 
-    print(type(file_type))
-
     file_type_list = ['jpg', 'png']
     if file_type not in file_type_list:
         return {"message": "jpg 또는 png 확장자를 가진 파일을 보내지 않음", "code": 411}, 411
 
+    try:
+        int(cloth_price)
+    except ValueError:
+        return {"message": "price 값에 문자열이 포함되어있음.", "code": 412}, 412
 
-    # url = f'Cloth/Image/{cloth_type}/{}'
+    path_dir = f'Data/Image/{cloth_type}'
+    file_list = os.listdir(path_dir)
+    file_list.sort()
+    print(file_list)
 
-    # audioFile.save(f"Data/Image/{cloth_type}/12341.{file_type}")
+    img_number = len(file_list) + 1
+    url = f'Cloth/Image/{cloth_type}/{img_number}.{file_type}'
+
+    for i in type_list:
+        sql = f'CREATE TABLE {i}List (' \
+               'User TEXT NOT NULL,'\
+               'Url TEXT NOT NULL,' \
+               'Title TEXT NOT NULL,' \
+               'Description TEXT NOT NULL,' \
+               'Price INT(11) NOT NULL,' \
+               'Size TEXT NOT NULL,' \
+               'FirstDate INT(11) NOT NULL,' \
+               'SellStatus BOOL DEFAULT 0 NOT NULL,' \
+               'ImageNumber INT(11) NOT NULL)'
+        try:
+            cursor.execute(sql)
+        except:
+            pass
+
+    sql = f'INSERT INTO {cloth_type}list ' \
+        '(User, Url, Title, Description, Price, Size, FirstDate, SellStatus, ImageNumber)' \
+        'VALUES()'
+
+    # audioFile.save(f"Data/Image/{cloth_type}/{img_number}.{file_type}")
 
     return f'{cloth_type}'
--- Server/Application/Cloth/Registration/views.py
+++ Server/Application/Cloth/Registration/views.py
@@ -9,10 +9,12 @@ from Application.Cloth.Registration import method
 cloth_namespace = api.namespace('Cloth', description='APIs for Request clothes')
 
 upload_parser = api.parser()
-upload_parser.add_argument('file', location='files', type='FileStorage', required=True)
-upload_parser.add_argument('description', type=str, required=True)
-upload_parser.add_argument('price', type=int,  required=True)
-upload_parser.add_argument('size', type=int, required=True)
+upload_parser.add_argument('file', location='files', type='FileStorage', required=True, help='등록할 옷에 대한 사진(jpg, png만)')
+upload_parser.add_argument('title', type=str, required=True, help='등록할 옷의 이름')
+upload_parser.add_argument('description', type=str, required=True, help='등록할 옷에 대한 추가 정보')
+upload_parser.add_argument('price', type=int,  required=True, help='등록할 옷의 가격에 대한 정보')
+upload_parser.add_argument('size', type=str, required=True, help='등록할 옷의 사이즈에 대한 정보')
+upload_parser.add_argument('first_date', type=int, required=True, help='등록할 옷을 처음으로 구매한 날짜')
 
 
 @cloth_namespace.route('/Register/<string:cloth_type>')
@@ -25,6 +27,7 @@ class Register(Resource):
             411: '전달한 파일의 확장자가 jpg, png 외의 파일임'
         }
     )
+    @jwt_required
     @api.expect(upload_parser)
     def post(self, cloth_type):
         return method.post(cloth_type)
