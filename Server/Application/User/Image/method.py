b76b1b072a5af11730d1623032f765e0dcfb579a
parkjinhong03 <jinhong0719@naver.com>
2019-09-28 23:36:40 +0900

    회원 프로필 사진 바꿔주는 API 구현 완료

--- Server/Application/User/Image/method.py
+++ Server/Application/User/Image/method.py
@@ -1,7 +1,61 @@
 import RequestParser
 from db_connect import db, cursor
+from flask_jwt_extended import get_jwt_identity
 from flask import send_file
+from flask_restplus import reqparse
+import werkzeug
+import os
 
 
 def get(img_name):
     return send_file(f'Data/Profile/{img_name}')
+
+
+def post():
+    _user = get_jwt_identity()
+
+    parse = reqparse.RequestParser()
+    parse.add_argument('file', type=werkzeug.datastructures.FileStorage, location='files')
+    args = parse.parse_args()
+    audioFile = args['file']
+
+    file_type = audioFile.filename.split('.')[1]
+
+    file_type_list = ['jpg', 'png']
+    if file_type not in file_type_list:
+        return {"message": "jpg 또는 png 확장자를 가진 파일을 보내지 않음", "code": 411}, 411
+
+    path_dir = f'Data/Profile'
+    file_list = os.listdir(path_dir)
+    file_list.sort()
+
+    img_number = 0
+    for i in file_list:
+        try:
+            int(i.split('.')[0])
+        except:
+            continue
+
+        img_number = int(i.split('.')[0]) + 1
+
+    sql = f'SELECT * FROM UserImage WHERE name="{_user}"'
+    cursor.execute(sql)
+
+    profile_data = cursor.fetchone()
+
+    if str(profile_data[1]).split('/')[2] == 'default.jpg':
+        sql = f'UPDATE UserImage SET url = "User/Profile/{img_number}.{file_type}"'
+        cursor.execute(sql)
+        db.commit()
+
+        audioFile.save(f"Data/Profile/{img_number}.{file_type}")
+
+    else:
+        os.remove(f'Data/Profile/{str(profile_data[1]).split("/")[2]}')
+        audioFile.save(f"Data/Profile/{str(profile_data[1]).split('/')[2].split('.')[0]}.{file_type}")
+
+        sql = f'UPDATE UserImage SET URL = "User/Profile/{str(profile_data[1]).split("/")[2].split(".")[0]}.{file_type}"'
+        cursor.execute(sql)
+        db.commit()
+
+    return {"message": "회원 사진 변경 완료", "code": 200}, 200
--- Server/Application/User/Image/views.py
+++ Server/Application/User/Image/views.py
@@ -8,6 +8,10 @@ from Application.User.Image import method
 
 cloth_namespace = api.namespace('User', description='APIs for Request users')
 
+post_parser = api.parser()
+post_parser.add_argument('JWT-Token', location='headers', required=True, help='회원 인증을 위해 User/login에서 받은 access_token을 Header에 포함해서 줘야함')
+post_parser.add_argument('file', location='files', type='FileStorage', required=True, help='등록할 옷에 대한 사진(jpg, png만)')
+
 
 @cloth_namespace.route('/Profile/<string:img_name>')
 class Image(Resource):
@@ -16,3 +20,17 @@ class Image(Resource):
     )
     def get(self, img_name):
         return method.get(img_name)
+
+
+@cloth_namespace.route('/Profile')
+class Profile(Resource):
+    @api.doc(
+        description='해당 유저의 프로필 이미지 사진을 변경해주는 API로, JWT-token과 바꾼 이미지 사진을 줘야한다.',
+        responses={
+            200: '회원 프로필 사진 변경 완료'
+        }
+    )
+    @api.expect(post_parser)
+    @jwt_required
+    def post(self):
+        return method.post()
